63d85d0d152444e74dfe970a6ca716dcaa1ef2fa
Danil Domnich <domni4.d2@yandex.ru>
2025-03-15 00:12:02 +0300

    Добавить сигнатуру для основных классов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,23 @@
 package org.example
 
+abstract class LibraryItem(val id: Int, var accessible: Boolean)
+
+class Book(id: Int, accessible: Boolean, var numberOfPages: Int, var author: String) : LibraryItem(id, accessible)
+{
+
+}
+
+class Newspaper(id: Int, accessible: Boolean, var issueNumber : Int) : LibraryItem(id, accessible)
+{
+
+}
+
+class Disk(id: Int, accessible: Boolean, var type: Int) : LibraryItem(id, accessible)
+{
+
+}
+
 fun main()
 {
-    
+
 }
