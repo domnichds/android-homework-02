ee0bca277bcd6711026a60a0c3cd063bd201218f
Danil Domnich <domni4.d2@yandex.ru>
2025-03-15 21:28:07 +0300

    Добавить реализацию методов вывода информации

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,26 @@
 package org.example
 abstract class LibraryItem(var id : Int, var name : String, val accessible : Boolean)
 {
-
+    fun getOneLineInfo() : String = "$name | Доступность: ${if (accessible) "да" else "нет"}"
+    abstract fun getInfo() : String
 }
 
-class Book(id : Int, name : String, accessible: Boolean, var numberOfPages : Int, var author : String) : LibraryItem(id, name, accessible)
+class Book(id : Int, name : String, accessible: Boolean, private var numberOfPages : Int, private var author : String) : LibraryItem(id, name, accessible)
 {
-
+    override fun getInfo() : String =
+        "Книга: $name ($numberOfPages стр.) автора: $author с id: $id | Доступность: ${if (accessible) "да" else "нет"}"
 }
 
-class Newspaper(id : Int, name : String, accessible: Boolean, var issueNumber : Int) : LibraryItem(id, name, accessible)
+class Newspaper(id : Int, name : String, accessible: Boolean, private var issueNumber : Int) : LibraryItem(id, name, accessible)
 {
-
+    override fun getInfo() : String =
+        "Выпуск: $issueNumber газеты $name с id: $id | Доступность: ${if (accessible) "да" else "нет"}"
 }
 
-class Disk(id : Int, name : String, accessible: Boolean, var type : Int) : LibraryItem(id, name, accessible)
+class Disk(id : Int, name : String, accessible: Boolean, private var type : Int) : LibraryItem(id, name, accessible)
 {
-    
+    override fun getInfo() : String =
+        "${if (type == 0) "CD" else "DVD"} $name | Доступность: ${if (accessible) "да" else "нет"}"
 }
 fun main() {
 
