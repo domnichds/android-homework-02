6eeda85e2efcd670177e332cb9cc65d88b429446
Danil Domnich <domni4.d2@yandex.ru>
2025-03-15 23:14:58 +0300

    Добавить интерфейсы и логку работы с пользователем (частично)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -22,6 +22,17 @@ class Disk(id: Int, name: String, accessible: Boolean, private var type: Int) : 
         "${if (type == 0) "CD" else "DVD"} $name | Доступность: ${if (accessible) "да" else "нет"}"
 }
 
+interface Borrowable {
+    fun takeHome() : String
+}
+
+interface ReadableInLibrary {
+    fun readInLibrary() : String
+}
+
+interface Returnable {
+    fun returnItem() : String
+}
 class LibraryManager(val libraryItemList: MutableList<LibraryItem>) {
     fun printList(type: Int) {
         when (type) {
@@ -31,9 +42,48 @@ class LibraryManager(val libraryItemList: MutableList<LibraryItem>) {
         }
     }
 
+
+
 }
 
 fun main() {
+    val libraryItems = mutableListOf<LibraryItem>(
+        Book(id = 1, name = "Преступление и наказание", accessible = true, numberOfPages = 500, author = "Федор Достоевский"),
+        Newspaper(id = 2, name = "Известия", accessible = true, issueNumber = 1525),
+        Disk(id = 3, name = "Брат", accessible = true, type = 1),
+        Book(id = 4, name = "Война и мир", accessible = true, numberOfPages = 1200, author = "Лев Толстой"),
+        Newspaper(id = 5, name = "Комсомольская правда", accessible = true, issueNumber = 1200),
+        Disk(id = 6, name = "Начало", accessible = true, type = 0),
+        Book(id = 7, name = "Мастер и Маргарита", accessible = true, numberOfPages = 400, author = "Михаил Булгаков"),
+        Disk(id = 8, name = "Зеркало", accessible = true, type = 1)
+    )
+    val library = LibraryManager(libraryItems)
+    println("Добро пожаловть в Library Manager!\n" +
+            "Навигация в меню происходит при помощи ввода цифр")
+    while (true) {
+        println("1 - Показать книги\n" +
+                "2 - Показать газеты\n" +
+                "3 - Показать диски\n" +
+                "4 - Выйти")
+        val mainChoice = readlnOrNull()?.toIntOrNull()
+        if (mainChoice == null || mainChoice !in 1..3) {
+            println("Неверный выбор!")
+        }
+        else if (mainChoice == 4) {
+            break
+        }
+        else {
+            while(true) {
+                library.printList(mainChoice)
+                println("1 - Взять домой\n" +
+                        "2 - Читать в читальном зале\n" +
+                        "3 - Показать подробную информацию\n" +
+                        "4 - Вернуть\n" +
+                        "5 - Выйти")
+                }
 
+            }
+        }
 }
+
 
