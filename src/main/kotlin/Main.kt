c4d3e06e30f78dcd5954d91c97219bb0737a56b9
Danil Domnich <domni4.d2@yandex.ru>
2025-03-22 13:54:05 +0300

    Поместит логику главного меню в класс InterfaceManager

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -28,8 +28,7 @@ abstract class LibraryItem(var id: Int, var name: String, var accessible: Boolea
         if (accessible) {
             println(successMessage)
             accessible = false
-        }
-        else {
+        } else {
             println(failureMessage)
         }
     }
@@ -39,8 +38,7 @@ abstract class LibraryItem(var id: Int, var name: String, var accessible: Boolea
         if (!accessible) {
             println(successMessage)
             accessible = true
-        }
-        else {
+        } else {
             println(failureMessage)
         }
     }
@@ -52,18 +50,24 @@ class Book(id: Int, name: String, accessible: Boolean, private var numberOfPages
         "Книга: $name ($numberOfPages стр.) автора: $author с id: $id | Доступность: ${if (accessible) "да" else "нет"}"
 
     override fun borrowItem() {
-        toggleAccessibility("Книга $name взята домой",
-            "Книга $name уже на руках")
+        toggleAccessibility(
+            "Книга $name взята домой",
+            "Книга $name уже на руках"
+        )
     }
 
     override fun readInLibrary() {
-        toggleAccessibility("Книга $name взята в читальный зал",
-            "Книга $name уже на руках")
+        toggleAccessibility(
+            "Книга $name взята в читальный зал",
+            "Книга $name уже на руках"
+        )
     }
 
     override fun returnItem() {
-        marksAsAccessible("Книга $name возвращена в библиотеку",
-            "Книга $name уже в библиотеке")
+        marksAsAccessible(
+            "Книга $name возвращена в библиотеку",
+            "Книга $name уже в библиотеке"
+        )
     }
 
 }
@@ -74,13 +78,17 @@ class Newspaper(id: Int, name: String, accessible: Boolean, private var issueNum
         "Выпуск: $issueNumber газеты $name с id: $id | Доступность: ${if (accessible) "да" else "нет"}"
 
     override fun readInLibrary() {
-        toggleAccessibility("Газета $name №$issueNumber взята в читальный зал",
-            "Газета $name №$issueNumber уже на руках")
+        toggleAccessibility(
+            "Газета $name №$issueNumber взята в читальный зал",
+            "Газета $name №$issueNumber уже на руках"
+        )
     }
 
     override fun returnItem() {
-        marksAsAccessible("Газета $name №$issueNumber возращена в библиотеку",
-            "Газета $name №$issueNumber уже в библиотеке")
+        marksAsAccessible(
+            "Газета $name №$issueNumber возращена в библиотеку",
+            "Газета $name №$issueNumber уже в библиотеке"
+        )
     }
 }
 
@@ -90,13 +98,17 @@ class Disk(id: Int, name: String, accessible: Boolean, private var type: Int) :
         "${if (type == 0) "CD" else "DVD"} $name | Доступность: ${if (accessible) "да" else "нет"}"
 
     override fun borrowItem() {
-        toggleAccessibility("${if (type == 0) "CD" else "DVD"} диск $name взят домой",
-            "${if (type == 0) "CD" else "DVD"} диск $name уже на руках")
+        toggleAccessibility(
+            "${if (type == 0) "CD" else "DVD"} диск $name взят домой",
+            "${if (type == 0) "CD" else "DVD"} диск $name уже на руках"
+        )
     }
 
     override fun returnItem() {
-        marksAsAccessible("${if (type == 0) "CD" else "DVD"} диск $name возращен в библиотеку",
-            "${if (type == 0) "CD" else "DVD"} диск $name уже в библиотеке")
+        marksAsAccessible(
+            "${if (type == 0) "CD" else "DVD"} диск $name возращен в библиотеку",
+            "${if (type == 0) "CD" else "DVD"} диск $name уже в библиотеке"
+        )
     }
 }
 
@@ -110,8 +122,8 @@ class LibraryManager(private val libraryItemList: MutableList<LibraryItem>) {
             3 -> libraryItemList.filterIsInstance<Disk>()
             else -> emptyList()
         }
-        list.forEachIndexed {index, item ->
-            println("${index+1}. ${item.getOneLineInfo()}")
+        list.forEachIndexed { index, item ->
+            println("${index + 1}. ${item.getOneLineInfo()}")
         }
     }
 
@@ -128,8 +140,95 @@ class LibraryManager(private val libraryItemList: MutableList<LibraryItem>) {
     }
 }
 
-fun main() {
+class InterfaceManager(private val library: LibraryManager) {
+    init {
+        println(
+            "Добро пожаловать в Library Manager!\n" +
+                    "Навигация в меню происходит при помощи ввода цифр"
+        )
+    }
 
+    private fun showMainMenu() {
+        println(
+            "1 - Показать книги\n" +
+                    "2 - Показать газеты\n" +
+                    "3 - Показать диски\n" +
+                    "4 - Выйти"
+        )
+    }
+
+    private fun showItemMenu() {
+        println(
+            "1 - Взять домой\n" +
+                    "2 - Читать в читальном зале\n" +
+                    "3 - Показать подробную информацию\n" +
+                    "4 - Вернуть\n" +
+                    "5 - Выйти"
+        )
+    }
+
+    fun mainMenu() {
+        while (true) {
+            showMainMenu()
+            when (val mainChoice = readlnOrNull()?.toIntOrNull()) { // Выбор списка, который следует показать
+                null, !in 1..4 -> println("Неверный выбор!")
+                4 -> break // Завершаем цикл и соответственно всю программу
+                else -> {
+                    itemMenu(mainChoice)
+                }
+            }
+        }
+    }
+
+    private fun itemMenu(type: Int) {
+        library.printList(type)
+        print("Введите номер объекта: ")
+        val itemChoice = readlnOrNull()?.toIntOrNull()
+        val item = library.getItem(type, itemChoice?.minus(1) ?: -1)
+        if (item == null) {
+            println("Неверный выбор!")
+        } else {
+            while (true) {
+                showItemMenu()
+                val localChoice = readlnOrNull()?.toIntOrNull()
+                if (localChoice == null || localChoice !in 1..5) {
+                    println("Неверный выбор!")
+                } else {
+                    if (handleItemChoice(localChoice, item)) break
+                }
+            }
+        }
+    }
+
+    // Метод работает с Item и возвращает true, если нужно выйти из цикла
+    private fun handleItemChoice(choice: Int, item: LibraryItem) : Boolean {
+        return when (choice) {
+            1 -> {
+                if (item is Borrowable) item.borrowItem()
+                else println("Этот объект нельзя взять домой")
+                false
+            }
+            2 -> {
+                if (item is ReadableInLibrary) item.readInLibrary()
+                else println("Этот объект нельзя читать в зале")
+                false
+            }
+            3 -> {
+                println(item.getInfo())
+                false
+            }
+            4 -> {
+                if (item is Returnable) item.returnItem()
+                else println("Этот объект нельзя вернуть")
+                false
+            }
+            5 -> true
+            else -> false
+        }
+    }
+}
+
+fun main() {
     // Тестовый список объектов библиотеки
     val libraryItems = mutableListOf<LibraryItem>(
         Book(id = 1, name = "Преступление и наказание", accessible = true, numberOfPages = 500, author = "Федор Достоевский"),
@@ -144,53 +243,8 @@ fun main() {
 
     // Создаем объект класса LibraryManager, передавая туда список объектов
     val library = LibraryManager(libraryItems)
-    println("Добро пожаловать в Library Manager!\n" +
-            "Навигация в меню происходит при помощи ввода цифр")
-    while (true) {
-        println("1 - Показать книги\n" +
-                "2 - Показать газеты\n" +
-                "3 - Показать диски\n" +
-                "4 - Выйти")
-        val mainChoice = readlnOrNull()?.toIntOrNull() // Выбор списка, который следует показать
-        if (mainChoice == null || mainChoice !in 1..4) {
-            println("Неверный выбор!")
-        }
-        else if (mainChoice == 4) {
-            break // Завершаем цикл и соответственно всю программу
-        }
-        else {
-            library.printList(mainChoice) // Выводим список объектов
-            print("Введите номер объекта: ")
-            val itemChoice = readlnOrNull()?.toIntOrNull()
-            val item = library.getItem(mainChoice, itemChoice?.minus(1) ?: -1)
-            if (item == null) {
-                println("Неверный выбор!")
-            }
-            else {
-                while (true) {
-                    println("1 - Взять домой\n" +
-                            "2 - Читать в читальном зале\n" +
-                            "3 - Показать подробную информацию\n" +
-                            "4 - Вернуть\n" +
-                            "5 - Выйти")
-                    val localChoice = readlnOrNull()?.toIntOrNull()
-                    if (localChoice == null || localChoice !in 1..5) {
-                        println("Неверный выбор!")
-                    } else {
-                        when (localChoice) {
-                            1 -> if (item is Borrowable) item.borrowItem()
-                            else println("Этот объект нельзя взять домой")
-                            2 -> if (item is ReadableInLibrary) item.readInLibrary()
-                            else println("Этот объект нельзя читать в зале")
-                            3 -> println(item.getInfo())
-                            4 -> if (item is Returnable) item.returnItem()
-                            else println("Этот объект нельзя вернуть")
-                            5 -> break
-                        }
 
-                    }
-                }
-            }
-        }
-    }
+    // Создаем объект класса InterfaceManager, передавая туда объект типа LibraryManager
+    val menu = InterfaceManager(library)
+    menu.mainMenu()
 }
